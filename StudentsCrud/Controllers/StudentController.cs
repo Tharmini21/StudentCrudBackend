40bcbdfe2dd7cfad38180f7c766ce0b519d701e5
Tharmini21 <tharminithaaru07@gmail.com>
2022-06-03 22:50:00 +0530

    Updated.

--- StudentsCrud/Controllers/StudentController.cs
+++ StudentsCrud/Controllers/StudentController.cs
@@ -11,6 +11,8 @@ using Smartsheet.Api.OAuth;
 using Microsoft.Extensions.Configuration;
 using StudentsCrud.Controllers;
 using StudentsCrud.Helper;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace StudentsCrud.Controllers
 {
@@ -24,14 +26,14 @@ namespace StudentsCrud.Controllers
 		}
 
 		static Dictionary<string, long> columnMap = new Dictionary<string, long>();
-		
+
 		[HttpGet("GetStudentDetails")]
 		public List<Student> GetStudentDetails()
 		{
 			List<Student> result = new List<Student>();
 			try
 			{
-				int pageSize = 20;
+				int pageSize = 25;
 				int page = 1;
 				Token token = new Token();
 				String accessToken = _iconfiguration["AccessToken"];
@@ -73,10 +75,10 @@ namespace StudentsCrud.Controllers
 			long sheetid = Convert.ToInt64(_iconfiguration["SheetId"]);
 			SmartsheetClient smartsheet = new SmartsheetBuilder().SetAccessToken(accessToken).Build();
 			Sheet sheet = smartsheet.SheetResources.GetSheet(sheetid, null, null, null, null, null, null, null);
-			Row row = smartsheet.SheetResources.RowResources.GetRow(sheetid,id,null,null);
+			Row row = smartsheet.SheetResources.RowResources.GetRow(sheetid, id, null, null);
 			return row;
 		}
-		
+
 		//[HttpPost("AddorUpdatestudent")]		
 		//public object AddorUpdatestudent(Student st)
 		//{
@@ -87,7 +89,7 @@ namespace StudentsCrud.Controllers
 		//		long sheetid = Convert.ToInt64(_iconfiguration["SheetId"]);
 		//		SmartsheetClient smartsheet = new SmartsheetBuilder().SetAccessToken(accessToken).Build();
 		//		Sheet sheet = smartsheet.SheetResources.GetSheet(sheetid, null, null, null, null, null, null, null);
-				
+
 		//		if (st == null)
 		//		{
 		//			return BadRequest("Given data is null");
@@ -155,7 +157,7 @@ namespace StudentsCrud.Controllers
 		//		Console.Write(ex.Message);
 		//	}
 		//	return status;
-			
+
 		//}
 		[HttpDelete("Delete/{id}")]
 		public string Delete(long id)
@@ -164,16 +166,16 @@ namespace StudentsCrud.Controllers
 			long sheetid = Convert.ToInt64(_iconfiguration["SheetId"]);
 			SmartsheetClient smartsheet = new SmartsheetBuilder().SetAccessToken(accessToken).Build();
 			Sheet sheet = smartsheet.SheetResources.GetSheet(sheetid, null, null, null, null, null, null, null);
-			smartsheet.SheetResources.RowResources.DeleteRows(sheetid, new long[] { id },true);
+			smartsheet.SheetResources.RowResources.DeleteRows(sheetid, new long[] { id }, true);
 			return "Record Deleted Successfully!!";
 		}
 
 		[HttpPost]
 		[Route("AddorUpdatestudent")]
-		public object AddorUpdatestudent([FromBody]Student student)
+		public object AddorUpdatestudent([FromBody] Student student)
 		{
 			string status = "";
-		
+
 			return status;
 
 		}
@@ -222,6 +224,9 @@ namespace StudentsCrud.Controllers
 						IList<Row> newRows = smartsheet.SheetResources.RowResources.AddRows(sheetid, new Row[] { rowA });
 						return status = "Data inserted Successfully";
 					}
+
+
+
 				}
 			}
 			catch (Exception ex)
@@ -240,7 +245,9 @@ namespace StudentsCrud.Controllers
 				long sheetid = Convert.ToInt64(_iconfiguration["SheetId"]);
 				SmartsheetClient smartsheet = new SmartsheetBuilder().SetAccessToken(accessToken).Build();
 				Sheet sheet = smartsheet.SheetResources.GetSheet(sheetid, null, null, null, null, null, null, null);
-
+				columnMap.Clear();
+				foreach (Column column in sheet.Columns)
+					columnMap.Add(column.Title, (long)column.Id);
 				if (st == null)
 				{
 					return BadRequest("Given data is null");
@@ -249,68 +256,18 @@ namespace StudentsCrud.Controllers
 				{
 					if (st.RowId != 0)
 					{
-						// Add rows to sheet
-						columnMap.Clear();
-						foreach (Column column in sheet.Columns)
-							columnMap.Add(column.Title, (long)column.Id);
-						var rows = new List<Row>();
-						if (st.RowId > 0)
-						{
-							var obj = StudentdetailByrowId(st.RowId);
-							if (obj.Id > 0)
-							{
-
-							}
-							long StudentNameColumnId = 0;
-							long GradeColumnId = 0;
-							foreach (var item in columnMap)
-							{
-								Console.WriteLine("Key " + item.Key + " Value " + item.Value);
-								//var GradeColumnId = GetColumnByTitle(sheet, "Grade", false)?.Id;
-								if (columnMap.ContainsKey("StudentName"))
-								{
-									StudentNameColumnId = item.Value;
-								}
-								else if (columnMap.ContainsKey("Grade"))
-								{
-									GradeColumnId = item.Value;
-								}
-								rows.Add(new Row
-								{
-									Id = st.RowId,
-									Cells = new List<Cell>
-									{
-										new Cell
-										{
-											ColumnId = 5776089125545860,
-											Value = st.StudentName
-
-										},
-										new Cell
-										{
-											ColumnId = 3899085337978756,
-											Value = st.Grade
-										}
-									}
-
-								});
-							}
-							var cellToUpdateB = new Cell
-							{
-								ColumnId = 5776089125545860,
-								Value = st.StudentName
-							};
-
-							// Identify row and add new cell values to it
-							var rowToUpdate = new Row
-							{
-								Id = st.RowId,
-								Cells = new Cell[] { cellToUpdateB }
-							};
-							//IList<Row> updatedRow = smartsheet.SheetResources.RowResources.UpdateRows(sheetid,rows);
-							IList<Row> updatedRow = smartsheet.SheetResources.RowResources.UpdateRows(sheetid, new Row[] { rowToUpdate });
-
-						}
+						Cell[] cell = new Cell[]
+						{ new Cell.UpdateCellBuilder(sheet.Columns[0].Id, st.StudentName).Build()
+							,new Cell.AddCellBuilder(sheet.Columns[1].Id, st.Grade).Build()
+							,new Cell.AddCellBuilder(sheet.Columns[2].Id, st.Address).Build()
+							,new Cell.AddCellBuilder(sheet.Columns[3].Id, st.City).Build()
+							,new Cell.AddCellBuilder(sheet.Columns[4].Id, st.Country).Build()
+							,new Cell.AddCellBuilder(sheet.Columns[5].Id, st.Postal).Build()
+							,new Cell.AddCellBuilder(sheet.Columns[6].Id, st.Phone).Build()
+							,new Cell.AddCellBuilder(sheet.Columns[7].Id, st.Email).Build()
+						};
+						Row row = new Row.UpdateRowBuilder(st.RowId).SetCells(cell).Build();
+						IList<Row> updatedRow = smartsheet.SheetResources.RowResources.UpdateRows(sheetid, new Row[] { row });
 					}
 				}
 			}
@@ -321,7 +278,109 @@ namespace StudentsCrud.Controllers
 			return "Updated Successfully";
 
 		}
-		
-	}
 	
+		[HttpGet("oauthurl")]
+		public object oauthurl()
+		{
+			OAuthFlow oauth = new OAuthFlowBuilder()
+				 .SetTokenURL("https://api.smartsheet.com/2.0/token")
+				 .SetAuthorizationURL("https://www.smartsheet.com/b/authorize")
+				 .SetClientId(_iconfiguration["SmartsheetClientId"])
+				 .SetClientSecret(_iconfiguration["SmartsheetClientSecret"])
+				 .SetRedirectURL("http://localhost:3000/callback")
+				 .Build();
+
+			string url = oauth.NewAuthorizationURL
+			(
+				new Smartsheet.Api.OAuth.AccessScope[]
+				{
+				Smartsheet.Api.OAuth.AccessScope.READ_SHEETS,
+				Smartsheet.Api.OAuth.AccessScope.WRITE_SHEETS,
+				Smartsheet.Api.OAuth.AccessScope.SHARE_SHEETS,
+				Smartsheet.Api.OAuth.AccessScope.DELETE_SHEETS,
+				Smartsheet.Api.OAuth.AccessScope.CREATE_SHEETS,
+				Smartsheet.Api.OAuth.AccessScope.READ_USERS,
+				Smartsheet.Api.OAuth.AccessScope.ADMIN_USERS,
+				Smartsheet.Api.OAuth.AccessScope.ADMIN_SHEETS,
+				Smartsheet.Api.OAuth.AccessScope.ADMIN_WORKSPACES,
+				},
+				"key=Test"
+			);
+			return url;
+		}
+
+		[HttpGet("Oauth")]
+		public string OauthCallback(string code, int expires_in, string state)
+		{
+			OAuthFlow oauth = new OAuthFlowBuilder()
+				.SetTokenURL("https://api.smartsheet.com/2.0/token")
+				.SetAuthorizationURL("https://www.smartsheet.com/b/authorize")
+				.SetClientId(_iconfiguration["SmartsheetClientId"])
+				.SetClientSecret(_iconfiguration["SmartsheetClientSecret"])
+				.SetRedirectURL("https://localhost:3000/callback")
+				.Build();
+
+			AuthorizationResult authResult = oauth.ExtractAuthorizationResult("http://localhost:3000/callback" + Request.QueryString.ToString());
+			Token token = oauth.ObtainNewToken(authResult);
+			return token.AccessToken;
+		}
+		[HttpPost("GetAccessToken")]
+		public string GetAccessToken(AccessTokenData data)
+		{
+			string response = string.Empty;
+			try
+			{
+				using (var Client = new HttpClient())
+				{
+					HttpContent content = new FormUrlEncodedContent(new Dictionary<string, string>
+					{
+					 {"grant_type", "authorization_code"},
+					 {"client_id", _iconfiguration["SmartsheetClientId"]},
+					 {"client_secret", _iconfiguration["SmartsheetClientSecret"]},
+					 {"code" ,data.Code }
+					});
+					HttpResponseMessage message = Client.PostAsync(AccessTokenData.TokenUrl, content).Result;
+					response = message.Content.ReadAsStringAsync().Result;
+				}
+
+				Newtonsoft.Json.Linq.JObject obj = JObject.Parse(response);
+				data.AuthToken = (string)obj["access_token"];
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+			return response;
+		}
+
+		[HttpPost("GetRefreshAccessToken")]
+		public string GetRefreshAccessToken(AccessTokenData data)
+		{
+			string response = string.Empty;
+			try
+			{
+				using (var Client = new HttpClient())
+				{
+					HttpContent content = new FormUrlEncodedContent(new Dictionary<string, string>
+					{
+					 {"grant_type", "refresh_token"},
+					 {"client_id", _iconfiguration["SmartsheetClientId"]},
+					 {"client_secret", _iconfiguration["SmartsheetClientSecret"]},
+					 {"refresh_token" ,data.RefreshToken }
+					});
+					HttpResponseMessage message = Client.PostAsync(AccessTokenData.TokenUrl, content).Result;
+					response = message.Content.ReadAsStringAsync().Result;
+				}
+				Newtonsoft.Json.Linq.JObject obj = JObject.Parse(response);
+				data.AuthToken = (string)obj["access_token"];
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+			return response;
+		}
+
+	}
+
 }
